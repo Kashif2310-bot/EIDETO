ccf8a42d62409c751f2bde8d7dd2187df24e3e01
MD MUJTABA KASHIF <1ms24ai033@msrit.edu>
2025-10-02 13:51:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2389404319-43b06",
+  "appId": "1:431442920639:web:396743d34d4d3e4aedf20a",
+  "apiKey": "AIzaSyDhqk2LT65aiqSAX6e2QDmM__ZWRd0FXrM",
+  "authDomain": "studio-2389404319-43b06.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "431442920639"
+};
